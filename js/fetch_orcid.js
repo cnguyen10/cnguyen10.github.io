08f2eec2e1b58793e7d8223057bcc5f035dbe35a
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-02-09 00:22:51 +0000

    Built site for gh-pages

--- js/fetch_orcid.js
+++ js/fetch_orcid.js
@@ -44,56 +44,52 @@ async function fetchPublications() {
 
 // Display publications in chunks (pagination)
 async function displayPublications(publications) {
-    const myNames = ['Cuong C. Nguyen', 'Nguyen, Cuong C.', 'Nguyen, C.C.', 'Cuong Nguyen', 'Nguyen, Cuong', 'Nguyen, C.']
+    // const myNames = ['Cuong C. Nguyen', 'Nguyen, Cuong C.', 'Nguyen, C.C.', 'Cuong Nguyen', 'Nguyen, Cuong', 'Nguyen, C.']
 
     const publicationList = document.getElementById('publication-list');
 
     // Display publications in batches of `pageSize`
     for (let i = displayedCount; i < Math.min(displayedCount + pageSize, publications.length); i++) {
-    const workSummary = publications[i]['work-summary'][0];
-    const title = workSummary['title']['title']['value'];
-    const putCode = workSummary['put-code'];  // Get the unique identifier for each work
+        const workSummary = publications[i]['work-summary'][0];
+        const title = workSummary['title']['title']['value'];
+        const putCode = workSummary['put-code'];  // Get the unique identifier for each work
 
-    let journalTitle = null;
-    if (workSummary['journal-title'] != null) {
-        journalTitle = workSummary['journal-title']['value'];
-    }
-    
-    let workUrl = null;
-    if (workSummary['url'] != null) {
-        workUrl = workSummary['url']['value'];
-    }
+        let journalTitle = null;
+        if (workSummary['journal-title'] != null) {
+            journalTitle = workSummary['journal-title']['value'];
+        } else {
+            journalTitle = 'Preprint'
+        }
 
-    // Fetch detailed information about the publication
-    const workDetailsUrl = `https://pub.orcid.org/v3.0/${orcidID}/work/${putCode}`;
-    const workDetailsResponse = await fetch(workDetailsUrl, {
-    headers: { 'Accept': 'application/json' }
-    });
-    const workDetails = await workDetailsResponse.json();
+        // Fetch detailed information about the publication
+        const workDetailsUrl = `https://pub.orcid.org/v3.0/${orcidID}/work/${putCode}`;
+        const workDetailsResponse = await fetch(workDetailsUrl, {
+        headers: { 'Accept': 'application/json' }
+        });
+        const workDetails = await workDetailsResponse.json();
 
-    // Get the list of contributors (authors)
-    let authors = workDetails.contributors.contributor
-        .map(contributor => contributor['credit-name']['value'])
-        .join(', ');
+        // Get the list of contributors (authors)
+        let authors = workDetails.contributors.contributor
+            .map(contributor => contributor['credit-name']['value'])
+            .join(', ');
 
-    // bold my name in the author list
-    for (let myName of myNames) {
-        authors = authors.replace(myName, `<strong style="color: MidnightBlue;">${myName}</strong>`);
-    }
-    
-    const pubYear = workDetails['publication-date']['year']['value'];
+        // // bold my name in the author list
+        // for (let myName of myNames) {
+        //     authors = authors.replace(myName, `<strong style="color: MidnightBlue;">${myName}</strong>`);
+        // }
+        
+        const pubYear = workDetails['publication-date']['year']['value'];
 
-    // Create list item for publication
-    const pubItem = document.createElement('li');
-    const pub_title = formatString(title);
-    let publication_text = `${authors} (${pubYear}) <strong>${pub_title}</strong>.`;
-    if (journalTitle != null) {
-        publication_text = publication_text + ` In <em>${journalTitle}.</em>`;
-    }
-    publication_text = publication_text + ` ${workUrl}`;
-    pubItem.innerHTML = publication_text;
-    pubItem.style.marginTop = "10px";
-    publicationList.appendChild(pubItem);
+        // Create list item for publication
+        const pubItem = document.createElement('li');
+        const pub_title = formatString(title);
+        let publication_text = `<span><strong>${pub_title}</strong> (${pubYear})</span><br /> <span><small>${authors}</small></span><br />`;
+        if (journalTitle != null) {
+            publication_text = publication_text + `<span><small><i>${journalTitle}</i></small></span>`;
+        }
+        pubItem.innerHTML = publication_text;
+        pubItem.style.marginTop = "10px";
+        publicationList.appendChild(pubItem);
     }
 
     // Update displayed count
@@ -101,9 +97,9 @@ async function displayPublications(publications) {
 
     // Show the "Display More" button if there are more publications to load
     if (displayedCount < publications.length) {
-    document.getElementById('load-more-btn').style.display = 'block';
+        document.getElementById('load-more-btn').style.display = 'block';
     } else {
-    document.getElementById('load-more-btn').style.display = 'none';
+        document.getElementById('load-more-btn').style.display = 'none';
     }
 }
 
